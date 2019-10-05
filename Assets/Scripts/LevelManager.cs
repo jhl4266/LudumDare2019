5360b636b8d5ab2c639f1ccae83727a09b2cc7b6
jhl4266 <jhl4266@g.rit.edu>
2019-10-04 20:10:08 -0700

    Added minimal tile grid generation and player controls

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,10 +6,23 @@ public class LevelManager : MonoBehaviour
 {
     public GameObject tile;
 
+    [Tooltip("Must be odd")]
+    public int gridSize;
+    private GameObject[,] grid;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        // Generate tile array
+        Mesh mesh = tile.GetComponentsInChildren<MeshFilter>()[0].sharedMesh;
+        grid = new GameObject[gridSize, gridSize];
+        int halfGridSize = gridSize / 2;
+        for (int z = -halfGridSize; z <= halfGridSize; z++) {
+            for (int x = -halfGridSize; x <= halfGridSize; x++)
+            {
+                Instantiate(tile, new Vector3(x * mesh.bounds.size.x, -mesh.bounds.size.y, z * mesh.bounds.size.z), Quaternion.identity);
+            }
+        }
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    private Mesh playerMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMesh = gameObject.GetComponentsInChildren<MeshFilter>()[0].sharedMesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            gameObject.transform.position += new Vector3(playerMesh.bounds.size.x, 0, 0);
+        } 
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            gameObject.transform.position -= new Vector3(playerMesh.bounds.size.x, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            gameObject.transform.position += new Vector3(0, 0, playerMesh.bounds.size.z);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            gameObject.transform.position -= new Vector3(0, 0, playerMesh.bounds.size.z);
+        }
+    }
+}
