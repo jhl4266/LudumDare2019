85929ea9cb6f2f420046b6d926331c5887df7b2b
mach19MC <mach19x@gmail.com>
2019-10-06 23:49:38 -0400

    Made Wave attack and created pixel image effect

---
+++ Assets/Materials/Pixelate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class Pixelate : MonoBehaviour
+{
+    public Material effectMaterial;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        Graphics.Blit(src, dst, effectMaterial);
+    }
+
+}
